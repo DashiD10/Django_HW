f35fda3d0075556fd513f979349437f92ce2c48a
Dashi <dorzhiev_dd@mail.ru>
2025-07-01 22:54:55 +1200

    HW39 Маршруты, базовый шаблон, включаемый компонент

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.landing, name='landing'),
-    path('thanks/', views.thanks, name='thanks'),
-    path('orders/', views.orders_list, name='orders_list'),
-    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('', include('main.urls')),  # Подключаем маршруты приложения
 ]
